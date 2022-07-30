f5a84c1fdede499b174d3838899a5da98352cac9
enajera1 <eriknajera013@gmail.com>
2022-07-29 20:30:25 -0500

    contact added

--- src/components/Nav/__tests__/index.test.js
+++ src/components/Nav/__tests__/index.test.js
@@ -1,3 +1,4 @@
+// __tests__/Nav.test.js with hard coded categories
 import React from 'react';
 import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
@@ -5,19 +6,17 @@ import Nav from '..';
 
 afterEach(cleanup);
 
-describe('Nav component', () => {
-  // baseline test
+describe('Nav component renders', () => {
   it('renders', () => {
     render(<Nav />);
   });
 
-  // snapshot test
   it('matches snapshot', () => {
     const { asFragment } = render(<Nav />);
-    // assert value comparison
+    
     expect(asFragment()).toMatchSnapshot();
   });
-});
+})
 
 describe('emoji is visible', () => {
   it('inserts emoji into the h2', () => {
@@ -25,14 +24,14 @@ describe('emoji is visible', () => {
 
   expect(getByLabelText('camera')).toHaveTextContent('📸');
   });
-})
+})  
 
 describe('links are visible', () => {
   it('inserts text into the links', () => {
-    // Arrange
     const { getByTestId } = render(<Nav />);
-    // Assert
+
     expect(getByTestId('link')).toHaveTextContent('Oh Snap!');
     expect(getByTestId('about')).toHaveTextContent('About me');
   });
+
 })
